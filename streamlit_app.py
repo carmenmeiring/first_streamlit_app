f205984f832a1657b1d86817227b7c4ac039128a
carmenmeiring <115042603+carmenmeiring@users.noreply.github.com>
2023-04-12 16:31:51 +0200

    Update streamlit_app.py
    Fix error re-run

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
      my_data_rows = get_fruit_load_list()
-     streamlit.dataframe(my_data_rows)
+     streamlit.dataframe(my_data_rows) 
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
 #Second Text Entry Box
