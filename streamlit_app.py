4cc5586459addd01cda7a301bdf14cad8e98c381
carmenmeiring <115042603+carmenmeiring@users.noreply.github.com>
2023-04-12 15:22:01 +0200

    Update streamlit_app.py
    Fix fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,4 @@ streamlit.dataframe(my_data_rows)
 #Second Text Entry Box
 # streamlit.header("Fruityvice Fruit Advice!")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+streamlit.write('Thanks for adding ', add_my_fruit)
