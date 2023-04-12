ecef993546fd5c2c40dbf7dc3a4bc5d54b9b68d4
carmenmeiring <115042603+carmenmeiring@users.noreply.github.com>
2023-04-12 14:31:40 +0200

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
-#tells Streamlit what libraries you plan to use in your project so it can add them in advance
 import snowflake.connector
