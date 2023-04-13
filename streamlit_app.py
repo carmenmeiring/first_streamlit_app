0fc31c99acf2c27b18696b7af9c5da58d818000f
carmenmeiring <115042603+carmenmeiring@users.noreply.github.com>
2023-04-13 08:28:53 +0200

    Update streamlit_app.py
    Fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,8 +96,6 @@ if streamlit.button('Add a Fruit to the List'):
      back_from_function = insert_row_snowflake(add_my_fruit)
      streamlit.text(back_from_function)
      
-streamlit.write('Thanks for adding ', add_my_fruit) 
-     
 #add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 #streamlit.write('Thanks for adding ', add_my_fruit) 
                 
